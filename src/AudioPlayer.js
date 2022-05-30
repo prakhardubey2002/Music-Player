d4df628a05fca36935518b6cc64c25a8b658de43
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-05-30 12:18:42 +0530

    css added-indexjs updated-audioplayejs added

---
+++ src/AudioPlayer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './main.css'
+import './input.css'
+
+const AudioPlayer = () => {
+  return (
+   <div className="audioplayer">
+       <div className="inside_content">
+           {/* header */}
+           {/* Action */}
+           {/* Playlist */}
+       </div>
+       <Controls/>
+   </div>
+  )
+}
+
+export default AudioPlayer
